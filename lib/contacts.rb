545ff1ce32d3c78f61dcbe8798d8d4df507fd767
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-14 20:39:18 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,8 @@ contacts = {
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
-    binding.pry
-  end
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
 end
